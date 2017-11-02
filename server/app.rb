c2b7a0c2005854ccca0369ab4f0014eda44b56e0
KatsuyaEndoh <katsuya@amac.local>
2017-11-02 16:58:37 +0900

    [update] set :environment, :production

--- server/app.rb
+++ server/app.rb
@@ -3,12 +3,15 @@ require 'pazucraft'
 require 'slim'
 require 'slim/include'
 
+set :environment, :production
+
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
   @ids = Dir.glob('public/img/*').map do |img_path|
     img_path.split('/').last
   end
+  @ids.sort
   slim :index
 end
 
@@ -26,10 +29,21 @@ post '/upload' do
     image_num = Dir.glob('public/img/*').size
     @input_file = "public/img/#{image_num}/in.jpg"
     @output_file = "public/img/#{image_num}/out.png"
+    noroshiro = 'public/assets/img/norishiro.png'
     Dir.mkdir "public/img/#{image_num}"
     File.open(@input_file, 'wb') do |f|
-      f.write(tempfile.read)
-      Pazucraft::generate @input_file, @output_file
+      if content_type == 'image/jpeg'
+        f.write(tempfile.read)
+        Pazucraft::generate @input_file, @output_file
+        image = Magick::Image.read(@output_file).first
+        norishiro_img = Magick::Image.read(noroshiro).first
+        image = norishiro_img.composite(image, 0, 0, Magick::OverCompositeOp)
+        image.write(@output_file)
+      else
+        image = Magick::Image.read(tempfile.path)[0]
+        image.write(@input_file)
+        Pazucraft::generate @input_file, @output_file
+      end
     end
     redirect :"link_to_image/#{image_num}"
   else
